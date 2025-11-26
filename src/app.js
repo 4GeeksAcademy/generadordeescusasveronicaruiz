694585abb92532d302107d3857c57a61d4fa1912
veronica860108 <veroruiba@gmail.com>
2025-11-26 11:49:17 +0000

    respuesta2-0

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const numeroAleatoriomath=(length)=>{
+  return Math.floor(Math.random()* length)
+}
 window.onload = function() {
 
 
@@ -12,30 +15,32 @@ window.onload = function() {
 
 
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let quien = who[Math.floor(Math.random()* who.length)];
+// let quien = who[Math.floor(Math.random()* who.length)];
+let quien = who[numeroAleatoriomath(who.length)];
 
 
 let action = ['ate', 'peed', 'crushed', 'broke'];
-let quehizo = action [Math.floor(Math.random()* action.length)];
-
+// let quehizo = action [Math.floor(Math.random()* action.length)];
+let queHizo = action[numeroAleatoriomath(action.length)];
 
 let what = ['my homework', 'my phone', 'the car'];
-let quefue = what [Math.floor(Math.random()* what.length)];
-
+// let quefue = what [Math.floor(Math.random()* what.length)];
+let queFue = what[numeroAleatoriomath(what.length)];
 
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-let cuando = when [Math.floor(Math.random()* when.length)];
+// let cuando = when [Math.floor(Math.random()* when.length)];
+let cuando = when[numeroAleatoriomath(when.length)];
 
 
 //let newButton = document.createElement("button");
 //newButton.innerText = "piensa";   
 //document.getElementById("piensa").innerHTML = quien + quehizo + quefue + cuando;
 
-let total = quien +" "+  quehizo + " " + quefue + " "+ cuando;
+let total = quien +" "+  queHizo + " " + queFue + " "+ cuando;
 
 console.log(total);
 
-document.getElementById("escusaboton").innerText = total
+document.getElementById("escusaboton").textContent = total
 
 
 
@@ -49,7 +54,7 @@ document.getElementById("escusaboton").innerText = total
 
   console.log("Hello Rigo from the console!");
 };
-
+window.onload()
 
 
 
